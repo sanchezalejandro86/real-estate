22b084c52efef3f05d3622559d466343265af4f1
Alejandro Sanchez <sanchezalejandro86@gmail.com>
2018-09-10 07:45:06 -0300

    landing con bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {BrowserModule, DomSanitizer} from "@angular/platform-browser";
+import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import { MatIconRegistry, MatIconModule } from '@angular/material';
 
 import {AppComponent} from "./app.component";
 import {SharedModule} from "./core/shared.module";
@@ -9,7 +7,6 @@ import {HeaderModule} from "./header/header.module";
 import {FooterModule} from "./footer/footer.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {HomeModule} from "./home/home.module";
-import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -18,19 +15,13 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     SharedModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    MatIconModule,
     HeaderModule,
     FooterModule,
     HomeModule,
-    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
-    matIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('./assets/mdi.svg'));
-  }
 }
